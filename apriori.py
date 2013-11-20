dc6325ddf5d5a007cabf6d0e960121a50d9922dc
asaini <abhi488@gmail.com>
2013-11-19 22:55:32 -0500

    attempt to pep8tify
    This code is badly written. Hope to do a complete re-write in the future

--- apriori.py
+++ apriori.py
@@ -1,109 +1,114 @@
 """
-Description	: Simple Python implementation of the Apriori Algorithm
-Author 		: Abhinav Saini(abhi488@gmail.com)
-Credits		: Cesare Zavattari(cesare@ctrl-z-bg.org) for making suggestions and refactoring code
+Description     : Simple Python implementation of the Apriori Algorithm
 
 Usage:
-	$python apriori.py -f DATASET.csv -s minSupport  -c minConfidence
-	
-	Eg.
-		$ python apriori.py -f DATASET.csv -s 0.15 -c 0.6
+    $python apriori.py -f DATASET.csv -s minSupport  -c minConfidence
 
+    $python apriori.py -f DATASET.csv -s 0.15 -c 0.6
 """
 
 import sys
-import re
 
-from itertools	 import chain, combinations
+from itertools import chain, combinations
 from collections import defaultdict
-from optparse 	 import OptionParser
+from optparse import OptionParser
+
 
 def subsets(arr):
     """ Returns non empty subsets of arr"""
-    return chain(*[combinations(arr,i + 1) for i,a in enumerate(arr)])
+    return chain(*[combinations(arr, i + 1) for i, a in enumerate(arr)])
 
 
 def returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet):
-	"""calculates the support for items in the itemSet and returns a subset of the itemSet 
-	each of whose elements satisfies the minimum support"""
-	_itemSet = set()
-	localSet = defaultdict(int)
+        """calculates the support for items in the itemSet and returns a subset
+       of the itemSet each of whose elements satisfies the minimum support"""
+        _itemSet = set()
+        localSet = defaultdict(int)
 
-	for item in itemSet:
-		for transaction in transactionList:
-			if item.issubset(transaction):
-				freqSet[item] 	+= 1
-				localSet[item]	+= 1
-	
-	for item,count in localSet.items():
-		support = float(count)/len(transactionList)
-		
-		if support >= minSupport:
-			_itemSet.add(item)
-	
-	return _itemSet
+        for item in itemSet:
+                for transaction in transactionList:
+                        if item.issubset(transaction):
+                                freqSet[item] += 1
+                                localSet[item] += 1
+
+        for item, count in localSet.items():
+                support = float(count)/len(transactionList)
+
+                if support >= minSupport:
+                        _itemSet.add(item)
+
+        return _itemSet
 
 
-
-def joinSet(itemSet,length):
-	"""Join a set with itself and returns the n-element itemsets"""
-	return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
+def joinSet(itemSet, length):
+        """Join a set with itself and returns the n-element itemsets"""
+        return set([i.union(j) for i in itemSet for j in itemSet if len(i.union(j)) == length])
 
 
 def getItemSetTransactionList(data_iterator):
-    transactionList	= list()
-    itemSet		= set()
+    transactionList = list()
+    itemSet = set()
     for record in data_iterator:
         transaction = frozenset(record)
         transactionList.append(transaction)
         for item in transaction:
-            itemSet.add(frozenset([item]))		# Generate 1-itemSets
+            itemSet.add(frozenset([item]))              # Generate 1-itemSets
     return itemSet, transactionList
 
 
 def runApriori(data_iter, minSupport, minConfidence):
     """
     run the apriori algorithm. data_iter is a record iterator
-    Return both: 
+    Return both:
      - items (tuple, support)
      - rules ((pretuple, posttuple), confidence)
     """
     itemSet, transactionList = getItemSetTransactionList(data_iter)
-    
-    freqSet		= defaultdict(int)
-    largeSet		= dict()				# Global dictionary which stores (key=n-itemSets,value=support) which satisfy minSupport
-    assocRules 		= dict()				# Dictionary which stores Association Rules
-    
-    oneCSet		= returnItemsWithMinSupport(itemSet, transactionList, minSupport, freqSet)
-    
-    currentLSet	= oneCSet
+
+    freqSet = defaultdict(int)
+    largeSet = dict()
+    # Global dictionary which stores (key=n-itemSets,value=support)
+    # which satisfy minSupport
+
+    assocRules = dict()
+    # Dictionary which stores Association Rules
+
+    oneCSet = returnItemsWithMinSupport(itemSet,
+                                        transactionList,
+                                        minSupport,
+                                        freqSet)
+
+    currentLSet = oneCSet
     k = 2
     while(currentLSet != set([])):
-        largeSet[k-1] 	= currentLSet
-        currentLSet 	= joinSet(currentLSet,k)
-        currentCSet 	= returnItemsWithMinSupport(currentLSet, transactionList, minSupport, freqSet)
-        currentLSet 	= currentCSet
+        largeSet[k-1] = currentLSet
+        currentLSet = joinSet(currentLSet, k)
+        currentCSet = returnItemsWithMinSupport(currentLSet,
+                                                transactionList,
+                                                minSupport,
+                                                freqSet)
+        currentLSet = currentCSet
         k = k + 1
 
     def getSupport(item):
             """local function which Returns the support of an item"""
             return float(freqSet[item])/len(transactionList)
 
-    toRetItems=[]
-    for key,value in largeSet.items():
-        toRetItems.extend([(tuple(item), getSupport(item)) 
+    toRetItems = []
+    for key, value in largeSet.items():
+        toRetItems.extend([(tuple(item), getSupport(item))
                            for item in value])
 
-    toRetRules=[]
-    for key,value in largeSet.items()[1:]:
+    toRetRules = []
+    for key, value in largeSet.items()[1:]:
         for item in value:
-            _subsets = map(frozenset,[x for x in subsets(item)])
+            _subsets = map(frozenset, [x for x in subsets(item)])
             for element in _subsets:
                 remain = item.difference(element)
-                if len(remain)>0:
+                if len(remain) > 0:
                     confidence = getSupport(item)/getSupport(element)
                     if confidence >= minConfidence:
-                        toRetRules.append(((tuple(element),tuple(remain)), 
+                        toRetRules.append(((tuple(element), tuple(remain)),
                                            confidence))
     return toRetItems, toRetRules
 
@@ -119,35 +124,46 @@ def printResults(items, rules):
 
 
 def dataFromFile(fname):
-	"""Function which reads from the file and yields a generator"""
-	file_iter = open(fname, 'rU')
-   	for line in file_iter:
-		line = line.strip().rstrip(',')				# Remove trailing comma
-		record = frozenset(line.split(','))
-		yield record
-
+        """Function which reads from the file and yields a generator"""
+        file_iter = open(fname, 'rU')
+        for line in file_iter:
+                line = line.strip().rstrip(',')                         # Remove trailing comma
+                record = frozenset(line.split(','))
+                yield record
 
 
 if __name__ == "__main__":
 
     optparser = OptionParser()
-    optparser.add_option('-f', '--inputFile', dest = 'input', help = 'the filename which contains the comma separated values', default=None)
-    optparser.add_option('-s', '--minSupport', dest='minS', help = 'minimum support value', default=0.15, type='float')
-    optparser.add_option('-c','--minConfidence', dest='minC', help = 'minimum confidence value', default = 0.6, type='float')
+    optparser.add_option('-f', '--inputFile',
+                         dest='input',
+                         help='filename containing csv',
+                         default=None)
+    optparser.add_option('-s', '--minSupport',
+                         dest='minS',
+                         help='minimum support value',
+                         default=0.15,
+                         type='float')
+    optparser.add_option('-c', '--minConfidence',
+                         dest='minC',
+                         help='minimum confidence value',
+                         default=0.6,
+                         type='float')
 
     (options, args) = optparser.parse_args()
 
     inFile = None
     if options.input is None:
-	    inFile = sys.stdin
+            inFile = sys.stdin
     elif options.input is not None:
-	    inFile = dataFromFile(options.input)
+            inFile = dataFromFile(options.input)
     else:
-	    print 'No dataset filename specified, system with exit\n'
-	    sys.exit('System will exit')
+            print 'No dataset filename specified, system with exit\n'
+            sys.exit('System will exit')
 
-    minSupport		= options.minS
-    minConfidence 	= options.minC
-    items, rules	= runApriori(inFile, minSupport, minConfidence)
+    minSupport = options.minS
+    minConfidence = options.minC
 
-    printResults(items,rules)
+    items, rules = runApriori(inFile, minSupport, minConfidence)
+
+    printResults(items, rules)
